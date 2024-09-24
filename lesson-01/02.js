0ba2552a3fba51b00dfd043140908384124b9867
kalinru <52463446+kalinru@users.noreply.github.com>
2024-09-24 16:15:14 +0500

    01 🍦 - Спринт 01, неделя 1 - JS  - 2 задание

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,7 @@
-// code
+let number1 = Math.floor(Math.random() * 100);
+
+const isOdd = number1 % 2 !== 0;
+
+const result = `${number1}:Это ${isOdd ? "не" : ""}четное число`;
+
+console.log(result);
