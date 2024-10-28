7b1335ebd18aad19a63a42cd4b73b5d8837c7245
kalinru <52463446+kalinru@users.noreply.github.com>
2024-10-28 12:34:30 +0500

    fix

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -2,6 +2,4 @@ let number1 = Math.floor(Math.random() * 100);
 
 const isOdd = number1 % 2 !== 0;
 
-const result = `[${number1}]: это ${isOdd ? "не" : ""}четное число`;
-
-console.log(result);
+console.log(`${number1} :это ${isOdd ? "не" : ""}четное число`);
