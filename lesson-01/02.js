2418dcd18d903972ea5356734b287d3c82f1688d
kalinru <52463446+kalinru@users.noreply.github.com>
2024-10-28 12:36:45 +0500

    fix

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,5 +1,7 @@
 let number1 = Math.floor(Math.random() * 100);
 
-const isOdd = number1 % 2 !== 0;
-
-console.log(`${number1} :это ${isOdd ? "не" : ""}четное число`);
+if (number1 % 2 !== 0) {
+  console.log(`${number1} :это нечетное число`);
+} else {
+  console.log(`${number1} :это четное число`);
+}
